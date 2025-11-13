159558d190b261da5e11fda25dedcffc961cc8ee
Leo-ksn <lkoshino@au.com>
2025-11-13 14:19:27 +0900

    Update main.js
    ファイルの相対パスの書き方の修正

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-import { setupTabs } from "./js/tabs.js";
-import { setupConverter } from "./js/converter.js";
-import { setupFlashcards } from "./js/flashcards.js";
+import { setupTabs } from "./tabs.js";
+import { setupConverter } from "./converter.js";
+import { setupFlashcards } from "./flashcards.js";
 
 document.addEventListener("DOMContentLoaded", () => {
   setupTabs();
